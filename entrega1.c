f74733c5b84ca727e5030b155b756a5ba14e0e30
Emmanuel  Huzehid Ascencio Trejo <115524511+Trexpapu@users.noreply.github.com>
2024-02-19 22:29:51 -0600

    Update entrega1.c

--- entrega1.c
+++ entrega1.c
@@ -46,12 +46,10 @@ int validar_comandos(char *comando){
     
 }
 int atiende_shell(char *comando, int *j, int *y) {
-    int ch = getch(); // Leer la entrada del usuario
-
-    if (ch != ERR) { // Comprobar si se presionó una tecla
-        comando[*j] = ch; // Almacenar la tecla en el comando
+    if (kbhit()) { // Comprobar si se presionó una tecla
+        comando[*j] = getch(); // Almacenar la tecla en el comando
         
-        if (ch == '\n') {//verificar si la tecla fue enter
+        if (comando[*j] == '\n') {//verificar si la tecla fue enter
             comando[*j] = '\0';//eliminar enter
             (*y)++; // Incrementar y para pasar a la siguiente línea
             return validar_comandos(comando);
@@ -80,21 +78,31 @@ void prints(int *y, char *comando){
 
 void mostrar_mensajes(int x, char *comando, int *j, int *y){
     int error_de_archivo=0;
+    int error_leyendo_archivo = 0;
     switch (x) {
+
             case 200:
+
+
                 mvprintw(40, 10, "                                                                                                               ");
                 mvprintw(40, 10, "Comando ejecutado exitosamente");
                 memset(comando, 0, (*j));
                 (*j) = 0;
-                error_de_archivo=verificar_archivo(archivo);
-                if (error_de_archivo==800){
+                error_de_archivo=verificar_archivo(archivo);//verficamos el archivo
+                if (error_de_archivo==800){//si el archivo se puede abrir correctamente
                     mvprintw(40, 10, "                                                                                                               ");
                     mvprintw(40, 10, "Error al abrir el archivo");
-                }else{
-                    leer_archivo(archivo);
+                }else{//si no regresa 800
+                    error_leyendo_archivo = leer_archivo(archivo);//leemos el archivo y vemos si no hay algun error de sintaxis, de ser asi mostraremos un mensaje
+                    if (error_leyendo_archivo == 405){
+                        mvprintw(40, 10, "                                                                                                               ");
+                        mvprintw(40, 10, "Error de sintaxis en el archivo");
+                    }
                 }
                 mvprintw((*y), 0, "PROMPT > %s", comando);
                 break;
+
+
                 
             case 405:
                 mvprintw(40, 10, "                                                                                                               ");
@@ -135,7 +143,7 @@ int main(void) {
     prints_procesador();
     while (recibe_valores != 10) {
         recibe_valores = atiende_shell(comando, &posicion_de_char, &nivel_de_prompt);
-        mostrar_mensajes(recibe_valores, comando, &posicion_de_char, &nivel_de_prompt);
+        mostrar_mensajes(recibe_valores, comando, &posicion_de_char, &nivel_de_prompt); // la funcion mostrar mensajes tambien llama las funciones de lectura de archivo
     }
 
     endwin();
